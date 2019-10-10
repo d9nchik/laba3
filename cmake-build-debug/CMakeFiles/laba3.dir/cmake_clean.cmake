0b6d4d0789cbe0cea0bc2264e0131296a32456d3
Danylo Halaiko <dan.galaiko@gmail.com>
2019-10-10 20:05:43 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/laba3.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/laba3.dir/main.cpp.o"
+  "laba3.pdb"
+  "laba3.exe"
+  "liblaba3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/laba3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
