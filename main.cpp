35ef9eeba1b8ca2f59703b7f63cca41a332475c4
Danylo Halaiko <dan.galaiko@gmail.com>
2019-10-12 14:09:12 +0300

    Final release with comment for teacher.

--- main.cpp
+++ main.cpp
@@ -6,11 +6,11 @@ using namespace std;
 int main() {
     setlocale(0, "russian");
     cout << "Введіть x для обчислення e^x: ";
-    float x;
+    float x; //Змінна для збреження значення в яку степінь підносимо
     cin >> x;
-    float adding = 1;
-    float number = 0;
-    int counter = 1;
+    float adding = 1; //Зміна, яка буде зберігати доданок після маніпуляції, який ми будемо додавати до число.
+    float number = 0;//Ініціалізуємо число до якого будемо додавати adding. Тут зберігається кінцевий результат.
+    int counter = 1;//Дізнаємося скільки кіл циклу пройшло для факторіалу.
     while (adding >= 0.0001) {
         number += adding;
         adding *= x;
